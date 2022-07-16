ff4b642dd361087207d0844725eb455ecfdb8dcf
cristianqsanchez <cristianquintero316@gmail.com>
2022-07-16 00:50:18 -0500

    update styles

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,9 +1,9 @@
 function Character({ character }) {
 	return (
 		<div className="text-center p-5">
-			<h3>{character.name}</h3>
 			<img className="img-fluid rounded-pill" src={character.image} alt={character.name} />
-      <p>{character.origin.name}</p>
+			<h5 className="card-title">{character.name}</h5>
+      <p className="card-text">{character.origin.name}</p>
 		</div>
 	)
 }
